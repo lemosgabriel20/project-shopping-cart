8f35110ce02b51a2d9c2914a393cd6fce5bd5108
Gabriel Lemos <jmjgb@outlook.com>
2022-11-22 05:27:54 -0300

    Listagem de produtos feita

--- src/main.js
+++ src/main.js
@@ -1,4 +1,13 @@
 import { searchCep } from './helpers/cepFunctions';
+import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+
+window.onload = async () => {
+  const products = await fetchProductsList('computador');
+  products.forEach((product) => {
+    document.querySelector('.products').appendChild(createProductElement(product));
+  });
+};
