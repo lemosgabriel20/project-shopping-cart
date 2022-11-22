ad5a31eb7166989d5d97714969d15cbddd21c084
Gabriel Lemos <jmjgb@outlook.com>
2022-11-22 04:06:02 -0300

    Função fetchProductsList finalizada

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -1,7 +1,15 @@
-export const fetchProduct = () => {
+const fetchProduct = () => {
   // seu código aqui
 };
 
-export const fetchProductsList = () => {
-  // seu código aqui
+const fetchProductsList = async (product) => {
+  try {
+    const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${product}`);
+    const data = await response.json();
+    return data.results;
+  } catch (error) {
+    throw new Error('Termo de busca não informado');
+  }
 };
+
+export { fetchProduct, fetchProductsList };
