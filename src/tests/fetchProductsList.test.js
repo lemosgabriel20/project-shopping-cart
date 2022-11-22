310bae3be5acd6aa0e35219049ac063a1980d70e
Gabriel Lemos <jmjgb@outlook.com>
2022-11-22 03:23:34 -0300

    TDD inicial feito

--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -5,17 +5,24 @@ import computadorSearch from './mocks/search';
 // implemente seus testes aqui
 describe('Teste a função fetchProductsList', () => {
   it('fetchProductsList é uma função', () => {
-    
+    expect(typeof fetchProductsList).toBe('function');
   });
 
   it('fetch é chamado ao executar fetchProductsList', () => {
-
+    return fetchProductsList('computador').then(data => {
+      expect(typeof data).toBe('promise');
+    });
   });
 
   it('fetch é chamado com o endpoint correto ao executar fetchProductsList', () => {
-
+    return fetchProductsList('computador').then(data => {
+      expect(data.name).toBe('computador');
+    });
   });
 
+  it('fetchProductsList não recebe argumentos e retorna um erro', () => {
+    return fetchProductsList('computador').rejects.toThrow('Termo de busca não informado');
+  });
   // it('...', () => {
   // });
 });
